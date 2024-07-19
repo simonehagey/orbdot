9705a011bd92b310b8f60ee87fc0608a6930e3ca
simonehagey <shagey@student.ubc.ca>
2024-07-19 13:05:05 -0600

    edit setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+
+with open("requirements.txt", "r") as fh:
+    requires = fh.readlines()
 
 setup(
     name="orbdot",
     version="0.1.0",
+    packages=find_packages(),
+    install_requires=requires,
     description="A set of tools for studying secular evolution of exoplanet orbits.",
     url="https://github.com/simonehagey/orbdot",
     author="Simone R. Hagey",
     author_email="shagey@phas.ubc.ca",
-    packages=["orbdot", "orbdot.tools", "orbdot.models", "orbdot.defaults"],
-    package_data={"orbdot": ['orbdot/defaults/*']}
+    package_data={"orbdot": ["defaults/*"]},
+    include_package_data=True
 )
