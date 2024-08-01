d746b2aa675032bd909b8e12c6e29958f6ac2ca8
Lukas Hergt <lthergt@posteo.de>
2024-07-31 19:53:39 -0700

    Version number (#1)
    * suggestion for author and version field at top of readme
    
    * fix Simones name
    
    * add readme to docs
    
    * fix version number in setup.py from 0.1.0 to 1.0.0
    
    * remove meta data from the docs `conf.py` file, better placed in the readme...
    
    * add version file
    
    * add `pyproject.toml` file to make more future proof
    
    * add templates for issues and pull requests that prompt the user to give some minimal information
    
    * add github action workflows to check that the version number is consistent in README and in the `__version__` file, and that it is unit incremented
    
    * delete `setup.py` now that we have a `pyproject.toml` file

---
+++ .github/workflows/workflows/check_version.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import sys
+import subprocess
+from packaging import version
+
+
+def run(*args):
+    """Run a bash command and return the output in Python."""
+    return subprocess.run(args, 
+                          text=True,
+                          stdout=subprocess.PIPE,
+                          stderr=subprocess.PIPE).stdout
+
+
+def unit_incremented(a, b):
+    """Check if a is one version larger than b."""
+    a = version.parse(a)
+    b = version.parse(b)
+    if a.pre is not None and b.pre is not None:
+        if a.pre[0] == b.pre[0]:
+            return a.pre[1] == b.pre[1]+1 and a.base_version == b.base_version
+        else:
+            return (a.pre[1] == 0 and a.pre[0] > b.pre[0]
+                    and a.base_version == b.base_version)
+    elif a.pre is not None:
+        return a.base_version > b.base_version and a.pre[1] == 0
+    elif b.pre is not None:
+        return a.base_version == b.base_version
+    else:
+        return (a.micro == b.micro+1 and
+                a.minor == b.minor and
+                a.major == b.major or
+                a.micro == 0 and
+                a.minor == b.minor+1 and
+                a.major == b.major or
+                a.micro == 0 and
+                a.minor == 0 and
+                a.major == b.major+1)
+
+
+README = "README.rst"
+vfile = "orbdot/__version__.py"
+current_version = run("cat", vfile)
+current_version = current_version.split("=")[-1].strip().strip("'")
+
+run("git", "fetch", "origin", "master")
+previous_version = run("git", "show", "remotes/origin/master:" + vfile)
+previous_version = previous_version.split("=")[-1].strip().strip("'")
+
+readme_version = run("grep", ":Version:", README)
+readme_version = readme_version.split(":")[-1].strip()
+
+if version.parse(current_version) != version.parse(readme_version):
+    sys.stderr.write("Version mismatch: {} != {}".format(vfile, README))
+    sys.exit(1)
+elif not unit_incremented(current_version, previous_version):
+    sys.stderr.write(("Version must be incremented by one:\n"
+                      "HEAD:   {},\n"
+                      "master: {}.\n"
+                      ).format(current_version, previous_version))
+    sys.exit(1)
+
