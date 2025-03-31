46cea9c6c5a9054cd986cad75f3585afebf1a916
Liam Gray <lgray@phas.ubc.ca>
2025-03-31 11:07:18 -0700

    build(pyproject): finish pyproject build setup and remove requirements.txt

--- orbdot/__init__.py
+++ orbdot/__init__.py
@@ -1,2 +1,22 @@
-__version__ = "1.0.0"
+"""orbdot.
 
+Submodules
+==========
+
+.. autosummary::
+    :toctree: _autosummary
+
+    defaults
+    models
+    tools
+"""
+
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("orbdot")
+except PackageNotFoundError:
+    # package is not installed
+    pass
+
+del version, PackageNotFoundError
--- orbdot/__version__.py
+++
@@ -1,4 +0,0 @@
-"""Version file for orbdot."""
-
-__version__ = '1.0.0'
-
